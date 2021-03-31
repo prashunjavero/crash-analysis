bd08c9b629b3982c7c9d74ed8a4f6f97cd6f7913
prashunjavero <javeriprashun2@gmail.com>
2021-03-31 19:59:33 +0200

    fixing the comments and linting

--- src/services/file_service.py
+++ src/services/file_service.py
@@ -7,8 +7,8 @@ from src.helper.user_helper import has_access
 from src.helper.file_helper import download_and_save
 
 def download_file(authenticated_user):
-    """ 
-    if the user is uthenticated and had the authorization download the file 
+    """
+    if the user is uthenticated and had the authorization download the file
     from the posted endpoint
     """
     if has_access(request, authenticated_user):
